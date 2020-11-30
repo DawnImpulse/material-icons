2f3523e1296421223b7d64a16938141342866089
Saksham Khurana <dawnimpulse@gmail.com>
2020-11-30 18:13:14 +0530

    feat: first basic version

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app style="background-color: #EEEEEE">
+    <v-container>
+      <!--filter-->
+      <v-row></v-row>
+
+      <!--icons-->
+      <!--list-->
+      <v-row justify="center" class="mt-6">
+        <v-col cols="8">
+          <v-card class="pt-8">
+            <v-row justify="center">
+              <v-col
+                align="center"
+                cols="2"
+                v-for="(icon, index) in icons"
+                v-bind:icon="icon"
+                v-bind:key="index"
+              >
+                <Icon :icon="icon"></Icon>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Icon from "@/components/Icon.vue";
+import meta from "@/lib/meta.json";
+
+export default Vue.extend({
+  name: "App",
+  components: { Icon },
+  data: () => ({
+    icons: [{}],
+    meta
+  }),
+  async created() {
+    //const files = await icons();
+    //console.log(icons);
+    const yes = await import("@mdi/js");
+    const nono = Object.values(yes);
+    this.icons = [];
+    nono.forEach((item, i) => {
+      this.icons.push({
+        icon: item.toString(),
+        meta: meta[i]
+      });
+    });
+    //delete this.icons[0];
+    //this.icons = [this.icons[0]];
+    //console.log(this.icons);
+    /*this.icons[0] = nono[0].toString();
+    console.log(this.icons);*/
+  }
+});
+</script>
---
+++ src/components/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tooltip bottom color="black">
+    <template v-slot:activator="{ on, attrs }">
+      <v-icon :color="color" v-bind="attrs" v-on="on">{{ icon.icon }}</v-icon>
+    </template>
+    <span>{{ icon.meta.name }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+import randomColor from "randomcolor";
+
+export default {
+  name: "Icon",
+  props: ["icon"],
+  data() {
+    return {};
+  },
+  computed: {
+    color() {
+      return randomColor({
+        luminosity: "dark"
+      });
+    }
+  }
+};
+</script>
